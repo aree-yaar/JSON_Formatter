002fbded6afc69f5a84ee02efb04f20a9995fedd
rtk707 <ritik652000@gmail.com>
2023-05-09 21:12:47 +0530

    CSS Enhancements and Added Toaster

--- formatter_util.py
+++ formatter_util.py
@@ -2,14 +2,25 @@ import json
 
 from flask import Response
 
+
 def beautify_json(input_text, indentation):
     return json.dumps(json.loads(input_text), indent=indentation)
+
 
 def minify_json(input_text):
     return json.dumps(json.loads(input_text))
 
+
 def download_file(content):
     return Response(content,
-            mimetype="application/json",
-            headers={"Content-Disposition":
-                    "attachment;filename=JSON_Formatter.txt"})
+                    mimetype="application/json",
+                    headers={"Content-Disposition":
+                             "attachment;filename=JSON_Formatter.txt"})
+
+
+def validate_json(jsonData):
+    try:
+        json.loads(jsonData)
+    except ValueError as err:
+        return False
+    return True

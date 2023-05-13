87d47742eaf552e363fce2810674a9cb3628de4e
mukund26 <m.agarwalhp@gmail.com>
2023-05-13 20:44:21 +0530

    upload func

--- app.py
+++ app.py
@@ -1,25 +1,38 @@
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, request, flash, redirect
 from utils.converter_util import toCSV, toYaml, toXML
 from utils.formatter_util import beautify_json, minify_json, download_file, validate_json
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+UPLOAD_FOLDER = '/uploads'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS	
+
+@app.route('/uploader', methods = ['GET', 'POST'])
+def upload_file():
+    if request.method == "POST":
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename=='':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            fileData = open(filename, 'r')
+            input_text = fileData.read()
+            return render_template('home.html', input=input_text)
 
 @app.route("/", methods=("GET", "POST"))
 def home():
     input_text = ""
     if request.method == "POST":
         input_text = request.form['input-text']
-        print(request.files)
-        if('file' in request.files):
-            print(request.files, 1)
-            file = request.files['file']
-            filename = secure_filename(file.filename)
-            print(filename)
-            return render_template("home.html", input = "ABCD")
-
-                
         if request.form['converter'] == 'CSV':
             return render_template("home.html", output = toCSV(input_text), input = input_text)
         elif request.form['converter'] == 'YAML':
